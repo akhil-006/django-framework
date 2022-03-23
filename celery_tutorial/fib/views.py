8fddb7b1e069a2cb2b8f4438fdee507b7a668b90
akhil agarwal <akhil_agarwal006@yahoo.in>
2022-03-23 12:18:30 +0530

    added pagination functionality in fibonacci website

--- celery_tutorial/fib/views.py
+++ celery_tutorial/fib/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views import View
 from .models import FibModel
 from .forms import FibForm
@@ -7,9 +8,26 @@ from .tasks import fib_task
 
 # Create your views here.
 class FibListView(View):
+    objects_per_page = 10
+
     def get(self, request):
-        objects = FibModel.objects.all()
-        return render(request, 'fib/calculationresult.html', {'objects': objects})
+        objects = FibModel.objects.all().order_by('date_created')
+        objects = Paginator(objects, self.objects_per_page)
+        # print('OBJECTS COUNT: ', objects.count)
+        try:
+            page_num = request.GET.get('page')
+            pages = objects.page(page_num)
+        except EmptyPage as ep:
+            print(f'Page not found: {ep}')
+            pages = objects.page(objects.num_pages)
+        except PageNotAnInteger as pgni:
+            print(f'Page not found: {pgni}')
+            pages = objects.page(1)
+
+        context = {
+            'objects': pages
+        }
+        return render(request, 'fib/calculationresult.html', context)
 
 
 def Back(request):
